5fd1c9550b7ec06765886fda1a213001521d2281
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-03-25 17:22:55 +0530

    Update default.rb

--- mount_splunk/attributes/default.rb
+++ mount_splunk/attributes/default.rb
@@ -1,6 +1,8 @@
 # Following are the attributes for mounting splunk.
 default['mount_splunk']['mount_point'] = '/opt/splunk'
-default['mount_splunk']['size'] = '10'
+default['mount_splunk']['device_id'] = '/dev/sda1'
+default['mount_splunk']['filesystem'] = 'ext4'
+default['mount_splunk']['size'] = '10G'
 default['mount_splunk']['user'] = 'splunk'
 default['mount_splunk']['group'] = 'splunk'
 default['mount_splunk']['permission'] = '0775'    
