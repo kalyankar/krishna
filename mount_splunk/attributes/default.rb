d121aebcb144553d4df704429176a9335e39630e
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-03-25 16:34:18 +0530

    commit attributes
    commit attributes

---
+++ mount_splunk/attributes/default.rb
@@ -0,0 +1,6 @@
+# Following are the attributes for mounting splunk.
+default['mount_splunk']['mount_point'] = '/opt/splunk'
+default['mount_splunk']['size'] = '10'
+default['mount_splunk']['user'] = 'splunk'
+default['mount_splunk']['group'] = 'splunk'
+default['mount_splunk']['permission'] = '0775'    
