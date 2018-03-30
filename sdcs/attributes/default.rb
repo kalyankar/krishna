349adb89ef393fc3c8dd096310ec2f47bf4043e9
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-03-30 08:17:23 +0530

    Create default.rb

---
+++ sdcs/attributes/default.rb
@@ -0,0 +1,6 @@
+default['sdcs']['package']['ofe']['rhel_6'] = ''
+default['sdcs']['package']['ofe']['rhel_7'] = ''
+default['sdcs']['package']['ofe']['windows'] = ''
+default['sdcs']['ofe]']['ssl_cert'] = ''
+default['sdcs']['ofe']['management_server'] = '’
+default['sdcs']['ofe']['install_opt'] = ''
