d152a5be6edf18bcab12e5503c74779342370bc6
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 19:34:32 +0530

    Update default.rb

--- install_asm/attributes/default.rb
+++ install_asm/attributes/default.rb
@@ -3,3 +3,4 @@ default['install_asm']['asm_lib'] = '/home/vagrant/oracleasmlib-2.0.12-1.el7.x86
 default['install_asm']['disks']   = ['/dev/sde', '/dev/sdf', '/dev/sdg', '/dev/sdp', '/dev/sdh', '/dev/sdi', '/dev/sdj', '/dev/sdk', '/dev/sdl', '/dev/sdm', '/dev/sdn', '/dev/sdo']
 default['install_asm']['user'] = 'ofeora'
 default['install_asm']['group'] = 'ofedba'
+default['install_asm']['asm_file'] = '/etc/init.d/oracleasm'
