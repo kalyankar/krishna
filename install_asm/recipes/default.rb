4de32a5b0c6e623d42362ea49df1912a748ac331
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 17:19:39 +0530

    Update default.rb

--- install_asm/recipes/default.rb
+++ install_asm/recipes/default.rb
@@ -1,1 +1,13 @@
+rpm_package node['install_asm']['asm_support'] do
+  source :#{node['install_asm']['asm_support']}
+  action :install
+end
 
+yum_package 'kmod-oracleasm' do
+  action :install
+end
+
+rpm_package node['install_asm']['asm_lib'] do
+  source :#{node['install_asm']['asm_lib']}
+  action :install
+end
