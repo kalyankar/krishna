61aca5528bbf4b11a42c36a8cc6719986f504c45
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 18:33:14 +0530

    Update default.rb

--- install_asm/attributes/default.rb
+++ install_asm/attributes/default.rb
@@ -1,2 +1,3 @@
 default['install_asm']['asm_support'] = '/home/vagrant/oracleasm-support-2.1.8-3.el7.x86_64.rpm'
 default['install_asm']['asm_lib'] = '/home/vagrant/oracleasmlib-2.0.12-1.el7.x86_64.rpm'
+default['install_asm']['disks']   = ['/dev/sde', '/dev/sdf', '/dev/sdg', '/dev/sdp', '/dev/sdh', '/dev/sdi', '/dev/sdj', '/dev/sdk', '/dev/sdl', '/dev/sdm', '/dev/sdn', '/dev/sdo']
