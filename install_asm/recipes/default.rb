a5e5a89c37c9fa301e1a370413671fa4eeb132aa
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 18:49:00 +0530

    Update default.rb

--- install_asm/recipes/default.rb
+++ install_asm/recipes/default.rb
@@ -11,3 +11,13 @@ rpm_package node['install_asm']['asm_lib'] do
   source :#{node['install_asm']['asm_lib']}
   action :install
 end
+
+node['install_asm']['disks'].each do |disk|
+  if `fdisk -l 2> /dev/null | grep '#{disk}'
+    bash "create partition" do
+      code "(echo n; echo p; echo 1; echo; echo; echo w) | fdisk #{disk}"
+      action :run
+      not_if {partprobe -d -s #{disk}}
+    end
+  end
+end
