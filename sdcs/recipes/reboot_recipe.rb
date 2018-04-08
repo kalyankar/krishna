ea5771a11ddcab7bc4dc571712be9b2f1db7907a
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 21:26:19 +0530

    Update reboot_recipe.rb

--- sdcs/recipes/reboot_recipe.rb
+++ sdcs/recipes/reboot_recipe.rb
@@ -1,3 +1,61 @@
+
+
+if node['platform_version'].to_i == 5
+
+  cookbook_file "/tmp/#{node['fim']['package']['rhel_5']}" do
+    source "rhel/#{node['fim']['package']['rhel_5']}"
+  end 
+
+  reboot 'reboot_server' do
+    action :nothing
+    reason 'System needs a restart after installation of SDCS'
+    delay_mins 2
+  end
+
+  bash "install_SYMCcsp" do
+    code <<-EOH
+    chmod +rx /tmp/#{node['fim']['package']['rhel_5']}
+    /tmp/#{node['fim']['package']['rhel_5']} -silent 
+    EOH
+    action :run
+    not_if "rpm -qa | grep SYMCcsp"
+    notifies :reboot_now, 'reboot[reboot_server]', :immediately	
+  end
+
+  bash "install_SYMCcsp_version" do
+    code <<-EOH
+    chmod +rx /tmp/#{node['fim']['package']['rhel_5']}
+    /tmp/#{node['fim']['package']['rhel_5']} -silent
+    EOH
+    action :run
+    not_if "rpm -q --queryformat \'%{VERSION}-%{release}\n\' SYMCcsp | grep  "#{node['fim']['package_version']['rhel_5']}"  > /dev/null"
+    notifies :reboot_now, 'reboot[reboot_server]', :immediately	
+  end
+
+  bash "install_SYMCsdcss" do
+    code <<-EOH
+    chmod +rx /tmp/#{node['fim']['package']['rhel_5']}
+    /tmp/#{node['fim']['package']['rhel_5']} #{node['fim']['install_opt']}
+    EOH
+    action :run
+    not_if "rpm -qa | grep SYMCsdcss"
+    notifies :reboot_now, 'reboot[reboot_server]', :immediately	
+  end
+
+  bash "install_SYMCsdcss_version" do
+    code <<-EOH
+    chmod +rx /tmp/#{node['fim']['package']['rhel_5']}
+    /tmp/#{node['fim']['package']['rhel_5']} -silent 
+    EOH
+    action :run
+    not_if "rpm -q --queryformat \'%{VERSION}-%{release}\n\' SYMCsdcss | grep  "#{node['fim']['package_version']['rhel_5']}"  > /dev/null"
+    notifies :reboot_now, 'reboot[reboot_server]', :immediately	
+  end
+
+end
+
+
+
 if node['platform_version'].to_i == 6
 
   cookbook_file "/tmp/#{node['fim']['package']['rhel_6']}" do
