1b3f1aa6c66b204058775d8f622d6260d10a95ad
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 17:11:24 +0530

    Create default.rb

---
+++ install_asm/attributes/default.rb
@@ -0,0 +1,2 @@
+default['install_asm']['asm_support'] = '/home/vagrant/oracleasm-support-2.1.8-3.el7.x86_64.rpm'
+default['install_asm']['asm_lib'] = '/home/vagrant/oracleasmlib-2.0.12-1.el7.x86_64.rpm'
