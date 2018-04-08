312a2a08cdcf079f77e086339809d83e200df077
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-08 19:21:38 +0530

    Update default.rb

--- install_asm/recipes/default.rb
+++ install_asm/recipes/default.rb
@@ -12,6 +12,10 @@ rpm_package node['install_asm']['asm_lib'] do
   action :install
 end
 
+bash "configure asm" do
+  code "(echo #{node['install_asm']['user']}; echo #{node['install_asm']['group']}; echo "y"; echo "y") | /etc/init.d/oracleasm configure"
+end
+
 node['install_asm']['disks'].each do |disk|
   if `fdisk -l 2> /dev/null | grep '#{disk}'`
     bash "create partition" do
