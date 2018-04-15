12013b32f5a35e45bee62e7a0ffd48a356f171f3
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-15 21:05:17 +0530

    Create default.rb

---
+++ hpsa/attributes/default.rb
@@ -0,0 +1,7 @@
+# Following are the attributes for HPSA Packages installation.
+
+default['hpsa']['share'] = '\\NAS4HPSA1\HPSA media windows\Packages'
+
+#default['hpsa']['share'] = '\\\\NAS4HPSA1\\HPSA media windows\\Packages'
+#default['hpsa']['share'] = "\\NAS4HPSA1\HPSA media windows\Packages"
+#default['hpsa']['share'] = "\\\\NAS4HPSA1\\HPSA media windows\\Packages"
