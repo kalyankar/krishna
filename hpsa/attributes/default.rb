b1291dc4d1ddc3ca8b6e41ff7f785b8b46ae838e
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-15 22:38:14 +0530

    Update default.rb

--- hpsa/attributes/default.rb
+++ hpsa/attributes/default.rb
@@ -16,3 +16,12 @@ default['hpsa']['hpsum'] = 'sppaug282016'
 
 default['hpsa']['tsmzip'] = 'TSM_7.1.4.zip'
 default['hpsa']['tsm'] = 'TSM_7.1.4'
+
+default['hpsa']['hdlmzip'] = 'HDLM_8.4.zip'
+default['hpsa']['hdlm'] = 'HDLM_8.4'
+
+default['hpsa']['netmonzip'] = 'netmon_w2k8_x64new.zip'
+default['hpsa']['netmon'] = 'netmon'
+
+default['hpsa']['tbmrzip'] = 'TBMR_7.1.3.zip'
+default['hpsa']['tbmr'] = 'TBMR_7.1.3'
