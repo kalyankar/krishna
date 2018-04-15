7e51e9808deec514f30cccbdbeaca8133fb0ba6b
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-15 22:33:39 +0530

    Update default.rb

--- hpsa/attributes/default.rb
+++ hpsa/attributes/default.rb
@@ -13,3 +13,6 @@ default['hpsa']['localpath'] = 'C:\build'
 
 default['hpsa']['hpsumzip'] = 'sppaug282016.zip'
 default['hpsa']['hpsum'] = 'sppaug282016'
+
+default['hpsa']['tsmzip'] = 'TSM_7.1.4.zip'
+default['hpsa']['tsm'] = 'TSM_7.1.4'
