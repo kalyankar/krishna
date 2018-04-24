8dd2f6a2f5880e4457996f020eb014a2eb974fab
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-04-24 22:05:40 +0530

    Update default.rb

--- install_ocm/attributes/default.rb
+++ install_ocm/attributes/default.rb
@@ -4,5 +4,5 @@ default['install_ocm']['share']='\\\\APSET2717\\Chef\\build\\Emulex_OCM'
 #default['install_ocm']['share']='\\unpiox03pn\intelsystems\Software\Disk Management\Emulex\HBA Drivers Emerging'
 #default['install_ocm']['share']='\\\\unpiox03pn\\intelsystems\\Software\\Disk Management\\Emulex\\HBA Drivers Emerging'
 
-default['hpsa']['localpath'] = "C:\\build"
-#default['hpsa']['localpath'] = "C:\build"
+default['install_ocm']['localpath'] = "C:\\build"
+#default['install_ocm']['localpath'] = "C:\build"
