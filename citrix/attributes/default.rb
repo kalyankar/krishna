776b447dd580756969675c1a4d46b820dae303cb
kalyankar <37770675+kalyankar@users.noreply.github.com>
2018-05-01 06:15:38 +0530

    Create default.rb

---
+++ citrix/attributes/default.rb
@@ -0,0 +1,26 @@
+default['citrix']['share'] = "\\\\NAS4HPSA1\\HPSA media windows\\Packages"
+
+default['citrix']['folder'] = "C:\\Citrix\\Installs"
+default['citrix']['ms_folder'] = "C:\\Citrix\\Installs\\Microsoft"
+default['citrix']['auth_folder'] = "C:\\Citrix\\Installs\\auth"
+default['citrix']['subinacl_folder'] = "C:\\Citrix\\Installs\\subinacl"
+default['citrix']['adobe_folder'] = "C:\\Citrix\\Installs\\Adobe"
+default['citrix']['snow_folder'] = "C:\\Citrix\\Installs\\snowinventory"
+default['citrix']['citrix_folder'] = "C:\\Citrix\\Installs\\Citrix"
+
+
+default['citrix']['dotnetzip'] = "PKG_Citrix_Microsoft_DotNETFramework_4.7.zip"
+default['citrix']['visualcppzip'] = "PKG_Citrix_Visual_C++2013-2017.zip"
+default['citrix']['vdazip'] = "VDAServerSetup_7_15_2000.zip"
+default['citrix']['authzip'] = "PKG_citrix_authware70.zip"
+default['citrix']['pdfzip'] = "Custom PDF Writer.zip"
+default['citrix']['subinaclzip'] = "PKG_Citrix_subinacl.zip"
+default['citrix']['adobezip'] = "PKG_Citrix_AcrobatReaderDC.zip"
+default['citrix']['adobereaderzip'] = "PKG_Citrix_AcrobatReaderDC_15.006.30033.zip"
+
+default['citrix']['AdobeReaderDCPatchzip'] = "PKG_Citrix_Adobe_ReaderDCPatch_15.006.30392.zip"
+default['citrix']['AdobeReaderDCPatchzip2'] = "PKG_Citrix_Adobe_ReaderDCPatch_15.006.30413.zip"
+default['citrix']['tcamzip'] = "TCAM-12.2.3.1-x64.zip"
+default['citrix']['snowzip'] = "PKG_CTX_SnowInventoryClient_3.7.4.zip"
+default['citrix']['citrixzip'] = "Citrix Receiver 14.9.1000.17 Package.zip"
+default['citrix']['swpzip'] = "SWP_IE11_Hardening_and_Active-Setup.zip"
